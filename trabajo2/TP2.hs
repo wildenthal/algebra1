9ce871a02aed4d9dc845dfd37d2aa6847d5c3e46
wildenthal <8503469+wildenthal@users.noreply.github.com>
2014-11-13 13:04:17 -0300

    Completa ejercicio 3; corrige indentacion instance Show; define show x = showP x

--- trabajo2/TP2.hs
+++ trabajo2/TP2.hs
@@ -5,14 +5,9 @@ data Proposicion = P
                  | Y Proposicion Proposicion
                  | O Proposicion Proposicion
                  | Imp Proposicion Proposicion
-                 instance Show Proposicion where
-		show P = "P"
-		show Q = "Q"
-		show R = "R"
-		show (No p) = "~"++(show p)
-		show (Y p q) = "(" ++ (show p) ++ " ^ " ++ (show q) ++ ")"
-		show (O p q) = "(" ++ (show p) ++ " v " ++ (show q) ++ ")"
-		show (Imp p q) = "(" ++ (show p) ++ " => " ++ (show q) ++ ")"
+
+instance Show Proposicion where
+ show x = showP x
 
 --Ejercicio 1
 atomoONegacion :: Proposicion -> Bool
@@ -21,6 +16,7 @@ atomoONegacion Q = True
 atomoONegacion R = True
 atomoONegacion (No _) = True
 atomoONegacion _ = False
+
 --Ejercicio 2
 showP :: Proposicion -> String
 showP P = "P"
@@ -32,7 +28,9 @@ showP (O p q) = "(" ++ (showP p) ++ " v " ++ (showP q) ++ ")"
 showP (Imp p q) = "(" ++ (showP p) ++ " => " ++ (showP q) ++ ")"
 
 --Ejercicio3
---ejercicio 3
 eliminarImplicaciones :: Proposicion -> Proposicion
-eliminarImplicaciones (Imp a b) = O (No(eliminarImplicaciones a)) (eliminarImplicaciones b)
+eliminarImplicaciones (Imp a b) = O (No (eliminarImplicaciones a)) (eliminarImplicaciones b)
+eliminarImplicaciones (O a b) = O (eliminarImplicaciones a) (eliminarImplicaciones b)
+eliminarImplicaciones (Y a b) = Y (eliminarImplicaciones a) (eliminarImplicaciones b)
+eliminarImplicaciones (No a) = No (eliminarImplicaciones a)
 eliminarImplicaciones a = a
