b8cb8ea6d5ae1ec82583c5213981946340d0c135
Matifceyn <sempe_100@hotmail.com>
2014-11-13 12:03:33 -0300

    Update TP2.hs

--- trabajo2/TP2.hs
+++ trabajo2/TP2.hs
@@ -5,6 +5,14 @@ data Proposicion = P
                  | Y Proposicion Proposicion
                  | O Proposicion Proposicion
                  | Imp Proposicion Proposicion
+                 instance Show Proposicion where
+		show P = "P"
+		show Q = "Q"
+		show R = "R"
+		show (No p) = "~"++(show p)
+		show (Y p q) = "(" ++ (show p) ++ " ^ " ++ (show q) ++ ")"
+		show (O p q) = "(" ++ (show p) ++ " v " ++ (show q) ++ ")"
+		show (Imp p q) = "(" ++ (show p) ++ " => " ++ (show q) ++ ")"
 
 --Ejercicio 1
 atomoONegacion :: Proposicion -> Bool
@@ -24,3 +32,7 @@ showP (O p q) = "(" ++ (showP p) ++ " v " ++ (showP q) ++ ")"
 showP (Imp p q) = "(" ++ (showP p) ++ " => " ++ (showP q) ++ ")"
 
 --Ejercicio3
+--ejercicio 3
+eliminarImplicaciones :: Proposicion -> Proposicion
+eliminarImplicaciones (Imp a b) = O (No(eliminarImplicaciones a)) (eliminarImplicaciones b)
+eliminarImplicaciones a = a
